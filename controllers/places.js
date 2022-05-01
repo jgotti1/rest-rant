af47bf1e225cc190a678bba4fd87e029dacc3878
jgotti1 <jgotti1@optonline.net>
2022-05-01 11:02:59 -0400

    added places view

--- controllers/places.js
+++ controllers/places.js
@@ -2,7 +2,24 @@ const router = require("express").Router();
 
 //GETS
 router.get("/", (req, res) => {
-  res.send("This Is GET /places");
+  let places = [
+    {
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisines: "Thai, Pan-Asian",
+      pic: "http://placekitten.com/250/250",
+    },
+    {
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisines: "Coffee, Bakery",
+      pic: "http://placekitten.com/250/250",
+    },
+  ];
+
+  res.render("../views/places/index", { places });
 });
 router.get("/new", (req, res) => {
   res.send("This Is GET /places/NEW");
