40c040a5ae98811f0fee2b352de9a0a394a9c12f
jgotti1 <jgotti1@optonline.net>
2022-05-02 21:19:43 -0400

    aded 404 page photo and home page image

--- views/error404.jsx
+++ views/error404.jsx
@@ -3,9 +3,10 @@ const Def = require("./default");
 function error404() {
   return (
     <Def>
-      <main>
+      <main className="main-page404">
         <h1>404: PAGE NOT FOUND</h1>
         <p>Oops, sorry, we can't find this page!</p>
+        <img className="img404" src="./images/sad.jpg" alt="sad clown" />
       </main>
     </Def>
   );
--- views/home.jsx
+++ views/home.jsx
@@ -1,10 +1,20 @@
 const React = require("react");
 const Def = require("./default");
+
 function home() {
   return (
     <Def>
-      <main>
+      <main className="home">
         <h1>HOME</h1>
+        <div>
+          <img className="food1" src="/images/food1.jpg" alt="Food1-Image" />
+        </div>
+        <div className="credit">
+          Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+        </div>
+        <a href="/places">
+          <button className="btn-primary">Places Page</button>
+        </a>
       </main>
     </Def>
   );
