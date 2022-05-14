09f507bcd589d181392c9aea0138045d912e805d
jgotti1 <jgotti1@optonline.net>
2022-05-14 09:32:46 -0400

    error flag

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,27 +2,16 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
-  let message = "";
-  if (data.message) {
-    message = <h4 className="alert-danger">{data.message}</h4>;
-  }
+  console.log(data.id);
   return (
     <Def>
       <main>
         <h1 className="dataPlace">{data.place.name}</h1>
-        {message}
         <img className="showPage" src={`${data.place.pic}`} alt={data.place.name} />
-        <h3>
-          Located in {data.place.city}, {data.place.state}
-        </h3>
         <h2 className="description">Description</h2>
-
-        <h3>{data.place.showEstablished()}</h3>
-        <h4>Serving {data.place.cuisines}</h4>
-
-        {/* <p className="description-details">
+        <p className="description-details">
           {data.place.name} is locaated in {data.place.city}, {data.place.state} and serves {data.place.cuisines}
-        </p> */}
+        </p>
         <hr />
         <div>
           <h2 className="comments">Comments</h2>
